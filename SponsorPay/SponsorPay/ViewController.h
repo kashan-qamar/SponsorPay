134df6817dd15a6903a6ac749480813b68101764
Kashan Qamar <kashanqamar@gmail.com>
2014-09-25 09:03:27 +0300

    base commit

---
+++ SponsorPay/SponsorPay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SponsorPay
+//
+//  Created by Kashan Qamar on 9/24/14.
+//  Copyright (c) 2014 Skyline Dynamics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
